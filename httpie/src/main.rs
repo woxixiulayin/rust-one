816c25e6120f044e1a0b2e42066f0cf688ab9782
haoke <haoke@i-i.ai>
2021-10-20 20:59:00 +0800

    add

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,10 +1,11 @@
+use std::str::FromStr;
+
 use clap::Parser;
-use anyhow::Result;
+use anyhow::{ anyhow, Result};
 use reqwest::Url;
 
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", author = "haoke")]
-
 struct Opts {
   #[clap(subcommand)]
   subcmd: SubCommand,
@@ -29,12 +30,41 @@ struct Get {
 struct Post {
   #[clap(parse(try_from_str = parse_url))]
   url: String,
-  body: Vec<String>,
+  #[clap(parse(try_from_str = parse_kv_pair))]
+  body: Vec<KvPair>,
+}
+
+#[derive(Debug)]
+struct KvPair {
+  k: String,
+  v: String,
+}
+
+impl FromStr for KvPair {
+  type Err = anyhow::Error;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    // 使用 = 进行split,这会得到一个迭代器
+    let mut split = s.split("=");
+    let err = || anyhow!(format!("Failed to parse {}", s));
+    Ok(Self {
+        // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
+        // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+        k: (split.next().ok_or_else(err)?).to_string(),
+        // 从迭代器中取第二个结果作为value
+        v: (split.next().ok_or_else(err)?).to_string(),
+      })
+  }
 }
 
 fn parse_url(s: &str) -> Result<String> {
   let _url: Url = s.parse()?;
   Ok(s.into())
+}
+
+// 因为我们为 KvPair 实现了 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+  Ok(s.parse()?)
 }
 
 fn main() {
